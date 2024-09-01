67e8241df0431aaf80c0f72431e3beb814866e42
Damilare <bolarinwaoluwadamilare91@gmail.com>
2024-09-01 07:48:56 +0100

    change OG Image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -14,7 +14,7 @@ export const contentType = "image/png";
 
 export default async function Image() {
   const logoSrc = await fetch(
-    new URL("../public/assets/Portfolio.png", import.meta.url)
+    new URL("../public/assets/newOG.png", import.meta.url)
   ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
